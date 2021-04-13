86f41ce4fbf4c4e0ec2044f28241a3ca82893fb7
Christina <christinapadilla.cp@gmail.com>
2021-04-13 15:19:04 -0500

    add user reducer

---
+++ client/src/store/reducers/AuthReducer.js
@@ -0,0 +1,54 @@
+import {
+  SUBMIT_REGISTRATION,
+  SUBMIT_LOGIN,
+  ADD_REGISTRATION,
+  ADD_LOGIN
+} from '../types'
+
+const iState = {
+  registration: {
+    username: '',
+    password: ''
+  },
+  login: {
+    username: '',
+    password: ''
+  },
+  reg_submitted: false,
+  login_submitted: false
+}
+
+const AuthReducer = (state = iState, action) => {
+  switch (action.type) {
+    case SUBMIT_REGISTRATION:
+      return {
+        ...state,
+        reg_submitted: !state.reg_submitted
+      }
+    case SUBMIT_LOGIN:
+      return {
+        ...state,
+        login_submitted: !state.login_submitted
+      }
+    case ADD_REGISTRATION:
+      return {
+        ...state,
+        registration: {
+          ...state.registration,
+          [action.payload.name]: action.payload.input
+        }
+      }
+    case ADD_LOGIN:
+      return {
+        ...state,
+        login: {
+          ...state.login,
+          [action.payload.name]: action.payload.input
+        }
+      }
+    default:
+      return { ...state }
+  }
+}
+
+export default AuthReducer
---
+++ client/src/store/reducers/UserReducer.js
@@ -0,0 +1,33 @@
+import {
+  GET_USERS,
+  TARGET_USER_ID,
+  GET_USER_BY_ID,
+  GET_THEMES_BY_USER,
+  GET_REVIEWSTHEMES_BY_USER
+} from '../types'
+
+const iState = {
+  all_users: [],
+  current_user_data: [],
+  selected_user_id: null,
+  selected_user_data: []
+}
+
+const UserReducer = (state = iState, action) => {
+  switch (action.type) {
+    case GET_USERS:
+      return { ...state, all_users: action.payload }
+    case TARGET_USER_ID:
+      return { ...state, selected_user_id: action.payload }
+    // case GET_USER_BY_ID: //! duplicate
+    //   return { ...state, selected_user_data: action.payload }
+    // case GET_THEMES_BY_USER: //! duplicate
+    //   return { ...state, selected_user_data: action.payload }
+    case GET_REVIEWSTHEMES_BY_USER:
+      return { ...state, selected_user_data: action.payload }
+    default:
+      return { ...state }
+  }
+}
+
+export default UserReducer
