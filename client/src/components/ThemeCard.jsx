c771dfe838655864dc0ae5943979cbec49ddbe5e
Christina <christinapadilla.cp@gmail.com>
2021-04-13 16:20:54 -0500

    add component skeletons

---
+++ client/src/components/ThemeCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ThemeCard = () => {
+  return <div>ThemeCard</div>
+}
+
+export default ThemeCard
---
+++ client/src/components/ThemeDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ThemeDetails = () => {
+  return <div>ThemeDetails</div>
+}
+
+export default ThemeDetails
