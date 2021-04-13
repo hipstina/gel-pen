359738dd671a7ba5e40d2cb824be96379eb0985f
Christina <christinapadilla.cp@gmail.com>
2021-04-13 14:26:11 -0500

    add theme actions

---
+++ client/src/store/actions/ThemeActions.js
@@ -0,0 +1,74 @@
+import {
+  __CreateNewTheme,
+  __GetAllThemes,
+  __GetThemeById,
+  __DeleteThemeById,
+  __UpdateLikeCount
+} from '../../services/ThemeServices'
+import {
+  GET_THEMES,
+  GET_THEME_BY_ID,
+  CREATE_THEME,
+  INCREMENT_LIKES,
+  DELETE_THEME_BY_ID
+} from '../types'
+
+export const CreateNewTheme = () => async (dispatch) => {
+  try {
+    const theme = await __CreateNewTheme()
+    dispatch({
+      type: CREATE_THEME,
+      payload: theme
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const GetAllThemes = () => async (dispatch) => {
+  try {
+    const themes = await __GetAllThemes()
+    dispatch({
+      type: GET_THEMES,
+      payload: themes
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const GetThemeById = (id) => async (dispatch) => {
+  try {
+    const theme = await __GetThemeById(id)
+    dispatch({
+      type: GET_THEME_BY_ID,
+      payload: theme
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const UpdateLikeCount = (id, req) => async (dispatch) => {
+  try {
+    const theme = await __UpdateLikeCount(id, req)
+    dispatch({
+      type: INCREMENT_LIKES,
+      payload: theme
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const DeleteThemeById = (id) => async (dispatch) => {
+  try {
+    const deleted = await __DeleteThemeById(id)
+    dispatch({
+      type: DELETE_THEME_BY_ID,
+      payload: deleted
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
