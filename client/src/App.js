b8e093a7dc4a5d2d4f43fcc297d56b0cd00aa4d3
Christina <christinapadilla.cp@gmail.com>
2021-04-13 19:08:47 -0500

    add react router and set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
+import React from 'react'
+import EditorPage from './screens/EditorPage'
+import Nav from './components/Nav'
+import ProfilePage from './screens/ProfilePage'
+import ThemePage from './screens/ThemePage'
+import Login from './components/Login'
+import Register from './components/Register'
+import { Switch, Route } from 'react-router-dom'
+
 import './styles/App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Nav />
+      <main>
+        <Switch>
+          <Route exact path="/" component={EditorPage} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/themes/:theme_id" component={ThemePage} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </main>
+    </div>
+  )
 }
 
 export default App
